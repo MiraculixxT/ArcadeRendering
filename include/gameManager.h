14a0593e5057bcfc8a37fe159e97a85898c48a7c
Miraculixx <julius.bartelheim@gmail.com>
2025-06-26 11:55:57 +0200

    Add include folder to build script

---
+++ include/gameManager.h
@@ -0,0 +1,16 @@
+//
+// Created by julius on 6/26/25.
+//
+
+#ifndef GAMEMANAGER_H
+#define GAMEMANAGER_H
+
+namespace arcader {
+
+class gameManager {
+
+};
+
+} // arcader
+
+#endif //GAMEMANAGER_H
---
+++ src/gameManager.cpp
@@ -0,0 +1,11 @@
+//
+// Created by julius on 6/26/25.
+//
+
+#include "gameManager.h"
+
+namespace arcader {
+
+
+
+} // arcader
